e688560159aef372a33daa202984be34cacabb9e
benfarice <imzoughene@outlook.com>
2018-08-18 18:02:44 +0100

    variables

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author youssed
+ *
+ */
+module tawsite {
+	
+	
+}
---
+++ src/tawsite/Variables.java
@@ -0,0 +1,23 @@
+package tawsite;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// main code goes here
+		
+		System.out.println("Hello, my name is Youssef Imzoughene");
+		System.out.println("Java is cool");
+		int a = 12;
+		int b = 45;
+		int c = a+b;
+		System.out.println("a : "+a+" b : "+b+" = "+c);
+		
+		double d = 45.09;
+		String name = "imzoughene youssef";
+		String str = "Java is cool";
+		System.out.println("my name is : "+name);
+
+
+	}
+
+}
